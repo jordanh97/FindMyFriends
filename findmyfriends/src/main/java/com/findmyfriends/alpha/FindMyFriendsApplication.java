4001e7b861bb390f474e6bf0f44ecdba714a01a1
jordanh97 <47331061+jordanh97@users.noreply.github.com>
2019-10-07 07:45:47 +0200

    Minor tweaks to ensure stability

--- findmyfriends/src/main/java/com/findmyfriends/alpha/FindMyFriendsApplication.java
+++ findmyfriends/src/main/java/com/findmyfriends/alpha/FindMyFriendsApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FindMyFriendsApplication {
 
@@ -16,5 +18,6 @@ public class FindMyFriendsApplication {
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+
 
 }
